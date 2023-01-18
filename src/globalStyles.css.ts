44562287b9c7500cad223c9a0ba5e90997b56100
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-18 09:38:01 -0500

    styles global styles app

--- src/globalStyles.css.ts
+++ src/globalStyles.css.ts
@@ -18,9 +18,9 @@ const GlobalStyle = createGlobalStyle`
   }
 
   body {
+    box-sizing: border-box;
+    padding: 0;
     margin: 0;
-    display: flex;
-    place-items: center;
     min-width: 320px;
     min-height: 100vh;
   }
