46904b8652df27b18c4db90001537e1f08915af1
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-18 09:37:26 -0500

    add wrapper to the app for routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import GlobalStyle from './globalStyles.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <>
+  <BrowserRouter>
     <GlobalStyle />
     <App />
-  </>
+  </BrowserRouter>
 );
