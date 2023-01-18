7b27425efeaf6e6c31d9dc1b2799b531f73fb836
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-18 09:36:49 -0500

    add lazy loading, provide store and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import './App.css';
+import { lazy, Suspense } from 'react';
+import { Provider } from 'react-redux';
+import { Route } from 'react-router-dom';
+import { publicRoutes } from './models';
+import MyStore from './redux/store';
+import { RoutesWithNotFound } from './utilities';
+
+const Home = lazy(() => import('./pages/Home/Home'));
+const Game = lazy(() => import('./pages/Game/Game'));
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <Provider store={MyStore}>
+      <Suspense fallback='loading...'>
+        <RoutesWithNotFound>
+          <Route path={'/'} element={<Home />} />
+          <Route path={`/${publicRoutes.GAME}`} element={<Game />} />
+        </RoutesWithNotFound>
+      </Suspense>
+    </Provider>
+  );
 }
 
 export default App;
