4d131104274abedcd710f68b75fc25bda2e0234d
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-18 09:35:58 -0500

    add components

---
+++ src/components/Cards/Cards.tsx
@@ -0,0 +1,13 @@
+import CardsWrapper from './CardsWrapper.css';
+
+function Cards() {
+  return (
+    <CardsWrapper>
+      <picture>
+        <img src='' alt='' width={100} />
+      </picture>
+      <h3>Name</h3>
+    </CardsWrapper>
+  );
+}
+export default Cards;
---
+++ src/components/Cards/CardsWrapper.css.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const CardsWrapper = styled.article`
+  width: 100px;
+  height: 120px;
+
+  picture image {
+    width: auto;
+    height: auto;
+  }
+
+  h3 {
+    text-align: center;
+  }
+`;
+
+export default CardsWrapper;
