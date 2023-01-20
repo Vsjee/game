d9da0229c41524c4f8b55c495efabf14bf0bc33d
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-19 21:21:50 -0500

    min update barrel

---
+++ src/pages/Game/Components/index.ts
@@ -0,0 +1,4 @@
+export { default as GameBoard } from './GameBoard/GameBoard';
+export { default as GameBoardWrapper } from './GameBoard/GameBoardWrapper.css';
+export { default as Timer } from './Timer/Timer';
+export { default as TimerWrapper } from './Timer/TimerWrapper.css';
