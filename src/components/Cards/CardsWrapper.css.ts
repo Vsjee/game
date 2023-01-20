a31200eff1e07ed0a7e06855755915c8f57b0658
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-19 21:20:57 -0500

    style card wrapper

--- src/components/Cards/CardsWrapper.css.ts
+++ src/components/Cards/CardsWrapper.css.ts
@@ -1,16 +1,40 @@
 import styled from 'styled-components';
 
 const CardsWrapper = styled.article`
-  width: 100px;
-  height: 120px;
+  padding: 0;
+  border: 2px solid white;
+  border-radius: 15px;
+  position: relative;
+  overflow: hidden;
+  background: url('https://i.pinimg.com/564x/2a/42/a6/2a42a62a1018d3150385647da07fa677.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: 0;
 
-  picture image {
-    width: auto;
-    height: auto;
+  button {
+    background: transparent;
+    border: none;
   }
 
-  h3 {
-    text-align: center;
+  img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    opacity: 0;
+  }
+
+  .hide {
+    cursor: pointer;
+  }
+
+  .show {
+    pointer-events: none;
+    img {
+      opacity: 1;
+    }
   }
 `;
 
