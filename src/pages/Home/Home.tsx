80595ebbed4e546fe7abc3a9d2ea20f032a74f28
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-19 21:20:04 -0500

    delete stuff in the home comp

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,29 +1,14 @@
 import { publicRoutes } from '@/models';
-import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { addCharacters } from '@/redux/state/characters';
-import { CharactersInfo } from '@/types';
-import { getCharacterData } from '@/utilities/getData';
+
 import HomeWrapper from './HomeWrapper.css';
 
 function Home() {
-  const dispatch = useDispatch();
-  let characters: CharactersInfo[] = [];
-
-  const handleClick = async () => {
-    let res = await getCharacterData();
-    let results: [] = res.results;
-    results.forEach((character: CharactersInfo) => {
-      characters.push({ name: character.name, image: character.image });
-    });
-    dispatch(addCharacters(characters));
-  };
-
   return (
     <HomeWrapper>
       <h1>Guessing Game</h1>
       <Link to={`/${publicRoutes.GAME}`}>
-        <button onClick={handleClick}>Start</button>
+        <button>Start</button>
       </Link>
     </HomeWrapper>
   );
